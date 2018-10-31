0e6e716af2ce67f3d50ac5847586af086f336e9b
Terion <mail@terion.name>
2018-10-31 15:45:25 +0200

    temporary

--- plugins/api.js
+++ plugins/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 Vue.use({
   install: function (Vue, options) {
     Vue.prototype.$api = axios.create({
-      baseURL: process.env.API_URL || 'http://localhost:8080',
+      baseURL: /*process.env.API_URL ||*/ 'http://localhost:8080',
       headers: {
         //Authorization: 'Bearer {token}'
       }
